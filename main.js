676ae9452e1851ba529a97b35e89a0f8cde78d2a
c4r7u <nicolo.carluccio3@gmail.com>
2021-11-16 09:15:14 +0100

    fixed

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-$('.header-right > a').click(function(){
-    $('.hamburger-menu').addClass('active');
-  })
-$('a.close').click(function(){
-    $('.hamburger-menu').removeClass('active');
-  }
-)
+const hamburgerIcon = document.querySelector('.fa-bars');
+
+
+hamburgerIcon.addEventListener('click',
+    function() {
+        const openBurger = document.querySelector('.hamburger-menu');
+        openBurger.classList.add('active');
+    }
+);
+
+const closeBurger = document.querySelector('.fa-times');
+
+
+closeBurger.addEventListener('click',
+    function() {
+        const openBurger = document.querySelector('.hamburger-menu');
+        openBurger.classList.remove('active');
+    }
+);
